72e00d659b95f81dbeeaa318d4dafdf586d70148
sanelemngadi <sanelemngadi17@gmail.com>
2022-04-12 15:14:24 +0200

    who am i hero sections

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import quizboard from "../../../assets/vectors/quizboard.svg";
+import { Link } from "react-router-dom";
 
 function Hero() {
     return (
@@ -9,8 +10,8 @@ function Hero() {
                     <h1 className='col_heading'>Welcome to Who Am I Quiz</h1>
 
                     <div className="cta">
-                        <a href='#' className='btn btn-secondary' >Contact Me</a>
-                        <a href='#' className='btn btn-primary' type="button">Watch videos {'>'}{'>'}</a>
+                        <Link to='#' className='btn btn-secondary' >Contact Me</Link>
+                        <Link to='#' className='btn btn-primary' type="button">Watch videos {'>'}{'>'}</Link>
                     </div>
                 </div>
                 <div className="col">
